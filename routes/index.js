565f2637ec9dcaa48e936e5cc5d53e932b2d6c0d
Afzalkhann <59650177+Afzalkhann@users.noreply.github.com>
2023-05-30 15:41:39 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ const router=express.Router()
 
 const homeController=require('../controllers/home')
 
+//home page router
 router.get('/',homeController.home)
+
+//project deatils while click on project name
 router.use('/project',require('./project'))
+
+//for creating issue
 router.use('/issue',require('./issue'))
 
 
